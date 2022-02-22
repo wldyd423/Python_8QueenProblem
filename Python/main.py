893381d1c75c4ef30d14fd6a356132c94d5e16c3
wldyd423 <jiyong423@g.skku.edu>
2022-02-22 18:08:36 +0900

    I dont remember nothing

--- Python/main.py
+++ Python/main.py
@@ -1,12 +1,32 @@
-from ast import List
-import random as rd
 import numpy as np
+import random as rd
 
-chessboard = np.zeros(8)
 
-print(chessboard)
+class EightQueens:
+    def __init__(self, size):
+        self.size = size
+        self.chessboard = np.zeros(self.size)
 
-for idx, position in enumerate(chessboard):
-    chessboard[idx] = rd.randrange(0, 8, 1)
+        for idx, position in enumerate(self.chessboard):
+            self.chessboard[idx] = rd.randrange(0, self.size, 1)
 
-print(chessboard)
+    def hit(self, x, y, i):
+        print(f"[{y}] Hit between line {x} and line {i}")
+
+    def evaluate(self):
+        print(self.chessboard)
+        for x, y in enumerate(self.chessboard):
+            for i in range(x+1, self.size):
+                if(self.chessboard[i] == y):
+                    self.hit(x, "ide", i)
+                    # print("hit")
+                elif(self.chessboard[i] == y+i-x):
+                    self.hit(x, "upp", i)
+                    # print("hit")
+                elif(self.chessboard[i] == y-i+x):
+                    self.hit(x, "low", i)
+                    # print("hit")
+
+
+e = EightQueens(8)
+e.evaluate()
